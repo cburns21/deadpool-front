6f22a9052d44e4f3549be167d3a08e0f494ee823
caroline burns <carolineburns@carolines-MacBook.local>
2019-03-28 14:59:27 -0600

    created header and buttons on homepage

---
+++ deadpool-front/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family:'Cinzel Decorative', cursive;;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #88a0a8;
+  background-color: #595752
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #88a0a8;
+}
+
+#nav a.router-link-exact-active {
+  color: #6d2828;
+}
+</style>
---
+++ deadpool-front/src/views/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="homepage">
+    <div class="header">
+      <img alt="Vue logo" class="img-responsive" src="../assets/ironThrone.png">
+      <HelloWorld msg="The Game of Thrones Deadpool"/>
+    </div>
+    <div class="buttons">
+      <button type="button" class="btn-lg">Create a Team</button>
+      <button type="button" class="btn-lg">Find Your Team</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+  .img-responsive {
+    display: flex;
+    max-height: 125px;
+    max-width: 150px;
+    float: left;
+  }
+  .header {
+    display: flex;
+    flex-grow: 2;
+    text-shadow: 5px 5px black;
+  }
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-grow: 3;
+
+  }
+  .btn-lg {
+    background-color: #6D2828;
+    margin-bottom: 20px;
+    align-items: center;
+    align-content: center;
+    width: 600px;
+    border: black;
+    box-shadow: 5px 5px;
+  }
+  .homepage {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    background-color: #595752; 
+    /* justify-content: center; */
+  }
+</style>
