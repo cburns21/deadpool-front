e7c79363cdf8f56d93b8d04b58b9c6811671cbfa
caroline burns <carolineburns@carolines-MacBook.local>
2019-03-28 16:28:35 -0600

    got audio to work

--- deadpool-front/src/views/Home.vue
+++ deadpool-front/src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="homepage">
-    <div class="header">
-      <img alt="Vue logo" class="img-responsive" src="../assets/ironThrone.png">
-      <HelloWorld msg="The Game of Thrones Deadpool"/>
-    </div>
-    <div class="buttons">
-      <button type="button" class="btn-lg">Create a Team</button>
-      <button type="button" class="btn-lg">Find Your Team</button>
-    </div>
+      <div class="header">
+        <img alt="Vue logo" class="img-responsive" src="../assets/ironThrone.png">
+        <HelloWorld msg="The Game of Thrones Deadpool"/>
+      </div>
+      <div class="buttons">
+        <button type="button" class="btn-lg">Create a Team</button>
+        <button type="button" class="btn-lg">Find Your Team</button>
+      </div>
+      <div id="audio" class="player-wrapper">
+        <audio controls>
+        <source src="../assets/song.mp3"
+        type="audio/mpeg"></audio>
+      </div>
   </div>
 </template>
 
@@ -20,6 +25,7 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
 <style scoped>
@@ -33,7 +39,39 @@ export default {
     display: flex;
     flex-grow: 2;
     text-shadow: 5px 5px black;
+    -webkit-animation: scale-up-top 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: scale-up-top 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+
   }
+@-webkit-keyframes scale-up-top {
+  0% {
+    -webkit-transform: scale(0.5);
+            transform: scale(0.5);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+  }
+}
+@keyframes scale-up-top {
+  0% {
+    -webkit-transform: scale(0.5);
+            transform: scale(0.5);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+  }
+}
+
   .buttons {
     display: flex;
     flex-direction: column;
@@ -58,4 +96,9 @@ export default {
     background-color: #595752; 
     /* justify-content: center; */
   }
+ 
+
 </style>
+
+	-webkit-animation: scale-up-top 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: scale-up-top 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
